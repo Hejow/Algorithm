609673dde279cb984e03095136dfdb4b3cc37bab
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-06-21 13:00:39 +0900

    220621

--- 14501.py
+++ 14501.py
@@ -1,10 +1,14 @@
-n = int(input())
-arr = []
-DP = [0] * n # i 번째 까지의 최대
+import sys
 
+n = int(input())
+timetable = [[0,0]]
 for i in range(n):
-    tmp = list(map(int, input().split()))
-    arr.append(tmp)
+    t, p = map(int, sys.stdin.readline().split())
+    timetable.append([t, p])
+DP = [0] * (n + 5)
+
+for i in range(1, n+1):
+    DP[i] = max(DP[i-1], DP[i])
+    DP[i + (timetable[i][0] - 1)] = max(DP[i + (timetable[i][0] - 1)], DP[i-1] + timetable[i][1])
     
-DP[0] = DP[1] = 0
-DP[2] = 10
+print(DP[n])
