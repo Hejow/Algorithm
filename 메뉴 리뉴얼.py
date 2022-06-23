a6551622a5fad539c135bc33152b423c75461c3e
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-06-23 16:16:08 +0900

    Update 메뉴 리뉴얼.py

--- 메뉴 리뉴얼.py
+++ 메뉴 리뉴얼.py
@@ -1,25 +1,24 @@
 from itertools import combinations
 
-orders = ["XYZ", "XWY", "WXA"]
-course = [2,3,4]
-combos = [{} for i in range(course[-1])]
-result = []
+def solution(orders, course):
+    answer = []
+    combos = [{} for i in range(course[-1])]
 
-for course in course:
-    for order in orders:
-        new_order = ''.join(sorted(order))
-        for combo in [''.join(i) for i in list(combinations(new_order, course))]:
-            if combo in combos[course-1]: combos[course-1][combo] += 1
-            else: combos[course-1][combo] = 1
-        
-for combo in combos:
-    for c in list(combo.keys()):
-        if combo[c] < 2: del combo[c]
+    for course in course:
+        for order in orders:
+            n_order = ''.join(sorted(order))
+            for combo in [''.join(i) for i in list(combinations(n_order, course))]:
+                if combo in combos[course-1]: combos[course-1][combo] += 1
+                else: combos[course-1][combo] = 1
 
-for combo in combos:
-    if combo:
-        max_val = max(combo.values())
-        for ele in combo:
-            if combo[ele] == max_val: result.append(ele)
-        
-print(sorted(result))
+    for combo in combos:
+        for c in list(combo.keys()):
+            if combo[c] < 2: del combo[c]
+
+    for combo in combos:
+        if combo:
+            max_val = max(combo.values())
+            for ele in combo:
+                if combo[ele] == max_val: answer.append(ele)
+
+    return sorted(answer)
