022e6c8f4543cafb91097b8620d5d5da4d855f79
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-06-26 20:59:27 +0900

    220626

--- 거리두기 확인하기.py
+++ 거리두기 확인하기.py
@@ -1,4 +1,14 @@
-places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+places = [
+    ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+    ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+    ["PXOPX",
+     "OXOXP",
+     "OXPOX",
+     "OXXOP",
+     "PXPOX"],
+    ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
+    ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
+]
 
 def check(place):
     people = []
@@ -9,18 +19,20 @@ def check(place):
                 
     for i in range(len(people)-1):
         x1, y1 = people[i]
-        for j in range(len(people)):
+        for j in range(i+1, len(people)):
             x2, y2 = people[j]
-            if abs(x1-x2) + abs(y1-y2) <= 2:
-                if x1 == x2 or y1 == y2:
-                    if place[x1][abs(y2-y1)-1] != 'X' or place[abs(x2-x1)-1][y1] != 'X':
-                        return 0
+            dist = abs(x1-x2) + abs(y1-y2)
+            if dist == 2:
+                if x2 == x1:
+                    if place[x1][y2-1] == 'O': return 0
+                elif y2 == y1:
+                    if place[x2-1][y1] == 'O': return 0
                 else:
-                    if place[x1][y2] != 'X' or place[x2][y1] != 'X':
-                        return 0
-        
+                    if place[x1][y2] == 'O' or place[x2][y1] == 'O': return 0
+            elif dist == 1: return 0
+    
     return 1
-                
+
 def solution(places):
     answer = []
     
@@ -28,3 +40,5 @@ def solution(places):
         answer.append(check(place))
     
     return answer
+
+print(solution(places))
