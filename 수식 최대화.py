1513491d347cc6272da813bef6c337aa2d0141e4
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-06-29 22:50:23 +0900

    220629

--- 수식 최대화.py
+++ 수식 최대화.py
@@ -1,22 +1,48 @@
-expression = "100-200*300-500+20"
+import itertools
 
 def toList(expression):
-    result = []
+    exp = []
     op = []
     tmp = ''
+    
     for ch in expression:
         if ch.isdigit(): tmp += ch
         else:
-            result.append(tmp)
-            result.append(ch)
-            op.append(ch)
+            exp.append(tmp)
+            exp.append(ch)
             tmp = ''
-    result.append(tmp)
-            
-    return result, set(op)
+            op.append(ch)
+    exp.append(tmp)
+    
+    return exp, list(set(op))
+
+def calc(exp, case):
+    list_ = []
+    stack = []
+    for i in range(len(exp)):
+        if exp[i].isdigit():list_.append(exp[i])
+        else:
+            if len(stack) == 0 : stack.append(exp[i])
+            else:
+                if case[stack[-1]] > case[exp[i]]: 
+                    list_.append(stack.pop())
+                    stack.append(exp[i])
+                else: stack.append(exp[i])
+    for i in range(len(stack)):
+        list_.append(stack.pop())
+        
+    return list_
+                    
 
 def solution(expression):
     answer = 0
-    exp, op = toList(expression)   
+    exp, op = toList(expression)
     
+    cases = []
+    for case in itertools.permutations(op, len(op)):
+        obj = {}
+        for c, i in zip(case, [3,2,1]):
+            obj[c] = i
+        cases.append(obj)
+        
     return answer
