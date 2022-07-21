736452c736ba625fa8dcbd97f99aecb30906b662
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-07-21 23:53:37 +0900

    220721

--- BOJ/2225.py
+++ BOJ/2225.py
@@ -1,12 +1,12 @@
 import sys
 
-N, K = map(int, sys.stdin.readline().split())
+n, k = map(int, sys.stdin.readline().split())
 mod = 1000000000
-table = [1]
-table += [0] * N
+DP = [0 for _ in range(n+1)]
+DP[0] = 1
 
-for _ in range(1, K+1):
-    for idx in range(1, N+1):
-        table[idx] = (table[idx] + table[idx-1])%mod
-
-sys.stdout.write(str(table[N]))
+for _ in range(1, k+1):
+    for i in range(1, n+1):
+        DP[i] = (DP[i] + DP[i-1]) % mod
+        
+print(DP[n])
