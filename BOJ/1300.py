54d31655d43307c585b32fa2bfed39f3c750b77d
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-07-26 04:48:58 +0900

    220726

--- BOJ/1300.py
+++ BOJ/1300.py
@@ -3,3 +3,19 @@ import sys
 n = int(sys.stdin.readline())
 k = int(sys.stdin.readline())
 
+left, right = 1, k
+
+while left <= right:
+    mid = (left + right) // 2
+    cnt = 0
+    
+    for i in range(1, n+1):
+        cnt += min(mid//i, n)
+        
+    if cnt >= k:
+        ans = mid
+        right = mid - 1
+    else: 
+        left = mid + 1
+    
+print(ans)
