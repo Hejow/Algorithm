c1ca8b86ce94247b9ec6d47870b5a7fcebe78a42
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-08-01 05:23:19 +0900

    220801

--- BOJ/14502(아직).py
+++ BOJ/14502(아직).py
@@ -1,13 +1,5 @@
-import sys, collections
+import sys
 input = sys.stdin.readline
 
-def bfs(n, m, board):
-    queue = collections.deque()
-
-# n, m = map(int, input().split())
-# board = [list(map(int, input().split())) for _ in range(n)]
-
-wall = 3
-cnt = 0
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
