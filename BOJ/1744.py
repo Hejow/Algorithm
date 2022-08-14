3337843f811ece10ecd681e9f87f59e3179155d3
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-08-15 01:16:21 +0900

    220815

--- BOJ/1744.py
+++ BOJ/1744.py
@@ -1,35 +1,29 @@
 N = int(input())
-positiveList = []
-negativeList = []
-oneList = []
+positive, negative = [], []
+one = []
 
 for _ in range(N):
     tmp = int(input())
-    if tmp > 1: positiveList.append(tmp)
-    elif tmp <= 0: negativeList.append(tmp)
-    else: oneList.append(tmp)
+    if tmp > 1: positive.append(tmp)
+    elif tmp <= 0: negative.append(tmp)
+    else: one.append(tmp)
     
-positiveList.sort(reverse=True)
-negativeList.sort()
+positive.sort(reverse=True)
+negative.sort()
 
-result = 0
+result = sum(one)
 
-if len(positiveList) % 2 == 0:
-    for i in range(0, len(positiveList), 2):
-        result += positiveList[i] * positiveList[i+1]
-else:
-    for i in range(0, len(positiveList)-1, 2):
-        result += positiveList[i] * positiveList[i+1]
-    result += positiveList[-1]
+def solution(array):
+    global result
     
-if len(negativeList) % 2 == 0:
-    for i in range(0, len(negativeList), 2):
-        result += negativeList[i] * negativeList[i+1]
-else:
-    for i in range(0, len(negativeList)-1, 2):
-        result += negativeList[i] * negativeList[i+1]
-    result += negativeList[-1]
+    l = len(array)
+    for i in range(0, l, 2):
+        if i == l-1:
+            result += array[i]
+            break
+        result += array[i] * array[i+1]
     
-result += sum(oneList)
+for arr in [positive, negative]:
+    solution(arr)
 
 print(result)
