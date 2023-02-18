52e8ed2ac310164174412ad5b18eb9dac4d59152
Moon He Jow <gmlwh124@naver.com>
2023-02-18 23:25:17 +0900

    230218

--- Softeer/로드 밸런서 트래픽 예측.py
+++ Softeer/로드 밸런서 트래픽 예측.py
@@ -2,6 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-requests = [0] * n
-balancer = {}
+requests = [0] * (n + 1)
 
+balancer = {}
+for i in range(1, n+1):
+    tmp = list(map(int, input().split()))
+    
+    if len(tmp) > 1: 
+        balancer[i] = tmp[1:]
+
+
+print(' '.join(map(str, requests[1:])))
