3d5ad75c1cf99fe0e7ab1d5fb8957cdb2e981171
Moon He Jow <gmlwh124@naver.com>
2023-04-23 19:20:55 +0900

    230423

---
+++ BOJ/6064.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+def solution(M, N, x, y):
+    return 0
+
+T = int(input())
+for _ in T:
+    M, N, x, y = map(int, input().split())
+    print(solution(M, N, x, y))
